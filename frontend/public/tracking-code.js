aa2b3097880e6185dbe24a2fb90e6ac074933cd2
Snehil Kumar <88778723+snehil002@users.noreply.github.com>
2026-08-02 21:01:18 +0530

    feat: add logic to capture SPA navigations

--- frontend/public/tracking-code.js
+++ frontend/public/tracking-code.js
@@ -1,64 +1,43 @@
 console.log("You are being tracked! For analytics! No private data!");
 
-let canSendData = false;
+let canSendData;
 let myTimeout;
 
 /* create initial user data */
 const userData = {
   "webId": "SUB001",
-  "currentUrlHost": location.origin,
-  "currentPagePath": location.pathname,
-  "visiOrOpenDateTime": "",
-  "timeOnPageInS": 0,
-  "trafficSource": document.referrer == "" ? "Direct" : document.referrer,
-  "deviceWidth": innerWidth
+  "currOrigin": location.origin,
+  "currUrl": location.href,
+  "initialTime": 0,
+  "finalTime": 0,
+  "timeDelta": 0,
+  "sourceUrl": document.referrer || "Direct",
+  "deviceWidth": screen.width
 };
 
 
-
-resetUserData();
-
-
-
-/* Capture Page Hide Or Show Event */
-document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState === "hidden") {
-    /* Prepare final user data */
-    if (canSendData) {
-      const finalTimeMillis = new Date().getTime();
-      const initialTimeMillis = userData["timeOnPageInS"];
-
-      userData["timeOnPageInS"] = (finalTimeMillis - initialTimeMillis) / 1000;
-
-      sendData();
-    }
-  }
-  else if (document.visibilityState === "visible") {
-    /* reset data */
-    resetUserData();
-  }
-});
-
-
-
-function resetUserData() {
+const resetInitialTime = () => {
   canSendData = false;
+  userData["initialTime"] = Date.now();
   
   clearTimeout(myTimeout);
   myTimeout = setTimeout(() => {
     canSendData = true;
   }, 10000);
-  
-  const initialTime = new Date();
+};
 
-  userData["visiOrOpenDateTime"] = initialTime.toISOString();
-  userData["timeOnPageInS"] = initialTime.getTime();
-}
 
+const resetSrcNCurrUrl = (prevUrl, currUrl) => {
+  if (userData["currUrl"] !== location.href) {
+    userData["sourceUrl"] = userData["currUrl"];
+    userData["currUrl"] = location.href;
+  }
+  userData["currOrigin"] = location.origin;
+};
 
 
 /* Send Data to Analytics Server */
-async function sendData () {
+const sendData = async () => {
   const host = "MY_SERVER_API_HERE";
   const URL = host + `/save-session-data`;
 
@@ -79,4 +58,51 @@ async function sendData () {
   catch(err) {
     console.error(err);
   }
-}
+};
+
+
+const sendExitData = () => {
+  if (canSendData) {
+    const finalTime = Date.now();
+    const initialTime = userData["initialTime"];
+
+    userData["timeDelta"] = (finalTime - initialTime) / 1000;
+    userData["finalTime"] = finalTime;
+
+    sendData();
+    // console.log(JSON.stringify(userData));
+  }
+};
+
+
+/* Capture Page Hide Or Show Event */
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    sendExitData();
+  }
+  else if (document.visibilityState === "visible") {
+    resetInitialTime();
+  }
+});
+
+
+/* Monkey-patch pushState & replaceState to capture SPA navigations */
+const patchHistory = function (type) {
+  const orig = history[type];
+  
+  return function () {
+    const result = orig.apply(this, arguments);
+    
+    sendExitData();
+    resetInitialTime();
+    resetSrcNCurrUrl();
+    
+    return result;
+  };
+};
+
+history.pushState = patchHistory('pushState');
+history.replaceState = patchHistory('replaceState');
+
+
+resetInitialTime();
--- subject/pages/_app.js
+++ subject/pages/_app.js
@@ -4,7 +4,7 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script src="http://localhost:3000/tracking-code.js" strategy="afterInteractive" />
+      <Script src="http://localhost:3000/tracking-code.js" strategy="beforeInteractive" />
       <Component {...pageProps} />
     </>
   );
